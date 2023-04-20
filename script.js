efc36e4383d6e17ab7715380e6321a39ab67c464
Jordan White <jwhite34@uw.edu>
2023-04-20 11:00:08 -0700

    Updating Page, adding photo

--- script.js
+++ script.js
@@ -1,36 +1,56 @@
-// Function to scroll smoothly to a specific element
-function smoothScrollTo(element, duration = 500) {
-  const targetPosition = element.offsetTop;
-  const startPosition = window.pageYOffset;
-  const distance = targetPosition - startPosition;
-  let startTime = null;
+document.addEventListener('DOMContentLoaded', () => {
+  const navLinks = document.querySelectorAll('.nav-link');
 
-  function animation(currentTime) {
-    if (startTime === null) startTime = currentTime;
-    const timeElapsed = currentTime - startTime;
-    const scrollAmount = easeInOutQuad(timeElapsed, startPosition, 
-distance, duration);
-    window.scrollTo(0, scrollAmount);
-    if (timeElapsed < duration) requestAnimationFrame(animation);
-  }
-
-  function easeInOutQuad(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
-  }
-
-  requestAnimationFrame(animation);
-}
-
-// Add click event listeners to navigation links
-document.querySelectorAll("nav a").forEach((link) => {
-  link.addEventListener("click", (event) => {
-    event.preventDefault();
-    const targetId = event.target.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
-    smoothScrollTo(targetElement);
+  navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const targetSection = document.querySelector(event.target.hash);
+      window.scrollTo({
+        top: targetSection.offsetTop - 50,
+        behavior: 'smooth'
+      });
+    });
   });
 });
 
+const githubUsername = 'JordanWhite34';
+
+// Fetch public repositories from GitHub
+async function fetchGitHubRepos() {
+  try {
+    const response = await fetch(`https://api.github.com/users/${JordanWhite34}/repos`);
+    const repos = await response.json();
+    displayRepos(repos);
+  } catch (error) {
+    console.error('Error fetching GitHub repositories:', error);
+  }
+}
+
+// Display repository information on the portfolio website
+function displayRepos(repos) {
+  const repoList = document.getElementById('repo-list');
+  repos.forEach(repo => {
+    const repoDiv = document.createElement('div');
+    repoDiv.classList.add('repo');
+
+    const repoName = document.createElement('h3');
+    repoName.textContent = repo.name;
+
+    const repoDescription = document.createElement('p');
+    repoDescription.textContent = repo.description || 'No description available';
+
+    const repoLink = document.createElement('a');
+    repoLink.href = repo.html_url;
+    repoLink.textContent = 'View on GitHub';
+    repoLink.target = '_blank';
+    repoLink.rel = 'noopener noreferrer';
+
+    repoDiv.appendChild(repoName);
+    repoDiv.appendChild(repoDescription);
+    repoDiv.appendChild(repoLink);
+    repoList.appendChild(repoDiv);
+  });
+}
+
+// Call the fetchGitHubRepos function when the page loads
+document.addEventListener('DOMContentLoaded', fetchGitHubRepos);
